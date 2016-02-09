67b72508386463e9e51a407cf4b11826dd497ae5
Taccoa <maml14@H473-0006.students.ad.bth.se>
2016-02-09 09:03:45 +0100

    K

--- 03-D3D/main.cpp
+++ 03-D3D/main.cpp
@@ -6,9 +6,10 @@
 #include "SimpleMath.h"
 #include "bth_image.h"
 #include <dinput.h>
+#include <vector>
+
 
 //http://www.braynzarsoft.net/viewtutorial/q16390-braynzar-soft-directx-11-tutorials
-//http://www.angelcode.com/dev/timefps/timefps.html
 
 using namespace DirectX::SimpleMath;
 using namespace DirectX;
@@ -32,7 +33,6 @@ ID3D11DepthStencilView* gDepthview = nullptr;
 ID3D11Texture2D* gDepthStencilBuffer = nullptr;
 
 ID3D11ShaderResourceView* gTextureView = nullptr;
-
 ID3D11Texture2D *gTexture = NULL;
 
 ID3D11Buffer* gVertexBuffer = nullptr;
@@ -88,6 +88,20 @@ void StartTimer();
 double GetTime();
 double GetFrameTime();
 
+//----------------------------------------------------------------
+bool isShoot = false;
+
+int ClientWidth = 0;
+int ClientHeight = 0;
+
+int score = 0;
+float pickedDist = 0.0f;
+
+void pickRayVector(float mouseX, float mouseY, XMVECTOR& pickRayInWorldSpacePos, XMVECTOR& pickRayInWorldSpaceDir);
+float pick(XMVECTOR pickRayInWorldSpacePos, XMVECTOR pickRayInWorldSpaceDir, std::vector<XMFLOAT3>& vertPosArray, std::vector<DWORD>& indexPosArray, XMMATRIX& worldSpace);
+bool PointInTriangle(XMVECTOR& triV1, XMVECTOR& triV2, XMVECTOR& triV3, XMVECTOR& point);
+//----------------------------------------------------------------
+
 struct VS_CONSTANT_BUFFER
 {
 	Matrix worldViewProj;
@@ -103,7 +117,6 @@ void CreateConstantBuffer()
 	desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 	desc.MiscFlags = 0;
 	desc.StructureByteStride = 0;
-
 
 	//Creating the Constant Buffer
 	gDevice->CreateBuffer(&desc, NULL, &gConstantBuffer);
@@ -538,7 +551,9 @@ bool InitDirectInput(HINSTANCE hInstance)
 	hr = DIKeyboard->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
 
 	hr = DIMouse->SetDataFormat(&c_dfDIMouse);			//Lets us tell the device what kind of input we are expecting
-	hr = DIMouse->SetCooperativeLevel(hwnd, DISCL_EXCLUSIVE | DISCL_NOWINKEY | DISCL_FOREGROUND);
+	//-------------------------------------------------
+	hr = DIMouse->SetCooperativeLevel(hwnd, DISCL_NONEXCLUSIVE | DISCL_NOWINKEY | DISCL_FOREGROUND);
+	//-------------------------------------------------
 
 	return true;
 }
@@ -582,22 +597,62 @@ void DetectInput(double time)
 
 	float speed = 15.0f * time;				//This is the speed our camera will move when we reposition it every frame
 
-	if (keyboardState[DIK_A] & 0x80)		//We check if it was the A key that was pressed
+	if (keyboardState[DIK_ESCAPE] & 0x80)	//We check if it was the ESCAPE key that was pressed
+	{
+		PostMessage(hwnd, WM_QUIT, 0, 0);	//Exits the window
+	}
+	if (keyboardState[DIK_A] & 0x80 || keyboardState[DIK_LEFT] & 0x80)		//We check if it was the A or Left key that was pressed
 	{
 		moveLeftRight -= speed;				//Moves the camera left
 	}
-	if (keyboardState[DIK_D] & 0x80)		//We check if it was the D key that was pressed
+	if (keyboardState[DIK_D] & 0x80 || keyboardState[DIK_RIGHT] & 0x80)		//We check if it was the D or Right key that was pressed
 	{
 		moveLeftRight += speed;				//Moves the camera right
 	}
-	if (keyboardState[DIK_W] & 0x80)		//We check if it was the W key that was pressed
+	if (keyboardState[DIK_W] & 0x80 || keyboardState[DIK_UP] & 0x80)		//We check if it was the W or Up key that was pressed
 	{
 		moveBackForward += speed;			//Moves the camera forward
 	}
-	if (keyboardState[DIK_S] & 0x80)		//We check if it was the S key that was pressed
+	if (keyboardState[DIK_S] & 0x80 || keyboardState[DIK_DOWN] & 0x80)		//We check if it was the S or Down key that was pressed
 	{
 		moveBackForward -= speed;			//Moves the camera back
 	}
+	//----------------------------------------------------------------------------
+	if (mouseCurrState.rgbButtons[0])
+	{
+		if (isShoot == false)
+		{
+			POINT mousePos;
+
+			GetCursorPos(&mousePos);
+			ScreenToClient(hwnd, &mousePos);
+
+			int mousex = mousePos.x;
+			int mousey = mousePos.y;
+
+			float tempDist;
+			float closestDist = FLT_MAX;
+			int hitIndex;
+
+			XMVECTOR prwsPos, prwsDir;
+			/*pickRayVector(mousex, mousey, prwsPos, prwsDir);*/
+
+
+
+			if (closestDist < FLT_MAX)
+			{
+				pickedDist = closestDist;
+				score++;
+			}
+			
+			isShoot = true;
+		}
+	}
+	if (!mouseCurrState.rgbButtons[0])
+	{
+		isShoot = false;
+	}
+	//----------------------------------------------------------------------------
 	if ((mouseCurrState.lX != mouseLastState.lX) || (mouseCurrState.lY != mouseLastState.lY)) //We check where the mouse are now
 	{
 		camYaw += mouseLastState.lX * 0.001f;
@@ -642,3 +697,119 @@ double GetFrameTime()
 
 	return float(tickCount) / countsPerSecond;
 }
+//----------------------------------------------------------------------
+void pickRayVector(float mouseX, float mouseY, XMVECTOR& pickRayInWorldSpacePos, XMVECTOR& pickRayInWorldSpaceDir)
+{
+	XMVECTOR pickRayInViewSpaceDir = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	XMVECTOR pickRayInViewSpacePos = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+	float PRVecX, PRVecY, PRVecZ;
+
+	//Transform 2D pick position on screen space to 3D ray in View space
+	/*PRVecX = ((( 2.0f * mouseX) / ClientWidth) - 1) / camProjection(0, 0);*/
+	/*PRVecY = (((2.0f * mouseY) / ClientHeight) - 1) / camProjection(1, 1);*/
+	PRVecZ = 1.0f;
+
+	/*pickRayInViewSpaceDir = XMVectorSet(PRVecX, PRVecY, PRVecZ, 0.0f);*/
+
+	XMMATRIX pickRayToWorldSpaceMatrix;
+	XMVECTOR matInvDeter;
+
+	pickRayToWorldSpaceMatrix = XMMatrixInverse(&matInvDeter, camView);
+
+	pickRayInWorldSpacePos = XMVector3TransformCoord(pickRayInViewSpacePos, pickRayToWorldSpaceMatrix);
+	pickRayInWorldSpaceDir = XMVector3TransformNormal(pickRayInViewSpaceDir, pickRayToWorldSpaceMatrix);
+}
+
+float pick(XMVECTOR pickRayInWorldSpacePos, XMVECTOR pickRayInWorldSpaceDir, std::vector<XMFLOAT3>& vertPosArray, std::vector<DWORD>& indexPosArray, XMMATRIX& worldSpace)
+{
+	for (int i = 0; i < indexPosArray.size() / 3; i++)
+	{
+		XMVECTOR tri1V1 = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+		XMVECTOR tri1V2 = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+		XMVECTOR tri1V3 = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+		XMFLOAT3 tV1, tV2, tV3;
+
+		tV1 = vertPosArray[indexPosArray[(i * 3) + 0]];
+		tV2 = vertPosArray[indexPosArray[(i * 3) + 1]];
+		tV3 = vertPosArray[indexPosArray[(i * 3) + 2]];
+
+		tri1V1 = XMVectorSet(tV1.x, tV1.y, tV1.z, 0.0f);
+		tri1V2 = XMVectorSet(tV2.x, tV2.y, tV2.z, 0.0f);
+		tri1V3 = XMVectorSet(tV3.x, tV3.y, tV3.z, 0.0f);
+
+		tri1V1 = XMVector3TransformCoord(tri1V1, worldSpace);
+		tri1V2 = XMVector3TransformCoord(tri1V2, worldSpace);
+		tri1V3 = XMVector3TransformCoord(tri1V3, worldSpace);
+
+		XMVECTOR U = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+		XMVECTOR V = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+		XMVECTOR faceNormal = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+		U = tri1V2 - tri1V1;
+		V = tri1V3 - tri1V1;
+
+		faceNormal = XMVector3Cross(U, V);
+		faceNormal = XMVector3Normalize(faceNormal);
+
+		XMVECTOR triPoint = tri1V1;
+
+		float tri1A = XMVectorGetX(faceNormal);
+		float tri1B = XMVectorGetY(faceNormal);
+		float tri1C = XMVectorGetZ(faceNormal);
+		float tri1D = (-tri1A*XMVectorGetX(triPoint) - tri1B*XMVectorGetY(triPoint) - tri1C* XMVectorGetZ(triPoint));
+
+		float ep1, ep2, t = 0.0f;
+		float planeIntersectX, planeIntersectY, planeIntersectZ = 0.0f;
+		XMVECTOR pointInPlane = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+		ep1 = (XMVectorGetX(pickRayInWorldSpacePos) * tri1A) + (XMVectorGetY(pickRayInWorldSpacePos) * tri1B) + (XMVectorGetZ(pickRayInWorldSpacePos) * tri1C);
+		ep2 = (XMVectorGetX(pickRayInWorldSpaceDir) * tri1A) + (XMVectorGetY(pickRayInWorldSpaceDir) * tri1B) + (XMVectorGetZ(pickRayInWorldSpaceDir) * tri1C);
+
+		if (ep2 != 0.0f)
+			t = -(ep1 + tri1D) / (ep2);
+
+		if (t > 0.0f)
+		{
+			planeIntersectX = XMVectorGetX(pickRayInWorldSpacePos) + XMVectorGetX(pickRayInWorldSpaceDir) * t;
+			planeIntersectY = XMVectorGetY(pickRayInWorldSpacePos) + XMVectorGetY(pickRayInWorldSpaceDir) * t;
+			planeIntersectZ = XMVectorGetZ(pickRayInWorldSpacePos) + XMVectorGetZ(pickRayInWorldSpaceDir) * t;
+
+			pointInPlane = XMVectorSet(planeIntersectX, planeIntersectY, planeIntersectZ, 0.0f);
+
+			if (PointInTriangle(tri1V1, tri1V2, tri1V3, pointInPlane))
+			{
+				return t / 2.0f;
+			}
+		}
+	}
+	return FLT_MAX;
+}
+
+bool PointInTriangle(XMVECTOR& triV1, XMVECTOR& triV2, XMVECTOR& triV3, XMVECTOR& point)
+{
+	XMVECTOR cp1 = XMVector3Cross((triV3 - triV2), (point - triV2));
+	XMVECTOR cp2 = XMVector3Cross((triV3 - triV2), (triV1 - triV2));
+	if (XMVectorGetX(XMVector3Dot(cp1, cp2)) >= 0)
+	{
+		cp1 = XMVector3Cross((triV3 - triV1), (point - triV1));
+		cp2 = XMVector3Cross((triV3 - triV1), (triV2 - triV1));
+		if (XMVectorGetX(XMVector3Dot(cp1, cp2)) >= 0)
+		{
+			cp1 = XMVector3Cross((triV2 - triV1), (point - triV1));
+			cp2 = XMVector3Cross((triV2 - triV1), (triV3 - triV1));
+			if (XMVectorGetX(XMVector3Dot(cp1, cp2)) >= 0)
+			{
+				return true;
+			}
+			else
+				return false;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+}
+//----------------------------------------------------------------------
